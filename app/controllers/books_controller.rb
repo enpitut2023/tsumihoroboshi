e2d1703353568389c7fb8b33a0d407af841797e5
y-hrtk <hrtk.ymd2002@gmail.com>
2023-07-20 10:11:12 +0900

    本の投稿、表示ができる

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,17 @@ class BooksController < ApplicationController
     def new
         @book_new = Book.new
     end
+    def create
+        @book_new = Book.new(book_params)
+        @book_new.save
+        redirect_to books_path
+    end
+    def index
+        @book_all = Book.all
+    end
+    private
+
+    def book_params
+        params.require(:book).permit(:title, :body)
+    end
 end
