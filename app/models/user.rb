4e7c6a56acc5e48fa708ea977ef2d0bae48839e5
nope0124 <kami-ryo0124@ezweb.ne.jp>
2023-07-21 15:52:45 +0900

    微修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
+  validates :email, uniqueness: true
   has_one_attached :image
   has_many :tsundokus, dependent: :destroy
 end
