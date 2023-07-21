3a7b0eebae5642126bc792d17eb0c1745e09b12b
nope0124 <kami-ryo0124@ezweb.ne.jp>
2023-07-21 11:05:07 +0900

    期限を設定、表示

--- app/controllers/tsundokus_controller.rb
+++ app/controllers/tsundokus_controller.rb
@@ -16,19 +16,22 @@ class TsundokusController < ApplicationController
     def edit
         @tsundoku = Tsundoku.find(params[:id])
     end
+
     def update
         @tsundoku = Tsundoku.find(params[:id])
         @tsundoku.update(tsundoku_params)
         redirect_to user_path(current_user.id)
     end
+
     def destroy
         @tsundoku = Tsundoku.find(params[:id])
         @tsundoku.destroy
         redirect_to user_path(current_user.id)
     end
+    
     private
 
     def tsundoku_params
-        params.require(:tsundoku).permit(:reading_status)
+        params.require(:tsundoku).permit(:reading_status, :deadline)
     end
 end
